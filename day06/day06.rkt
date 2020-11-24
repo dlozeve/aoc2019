629ff3780df5c1d8efbc780ca5af05f7a7d433a9
Dimitri Lozeve <dimitri@lozeve.com>
2020-11-24 21:22:45 +0100

    Day 6 part 2

--- day06/day06.rkt
+++ day06/day06.rkt
@@ -9,10 +9,15 @@
      (define g (unweighted-graph/directed '()))
      (for ([line (in-lines)])
        (define bodies (string-split line ")"))
-       (add-directed-edge! g (car bodies) (cadr bodies)))
+       (add-edge! g (car bodies) (cadr bodies)))
      g)))
 
 (define (part1 filename)
   (define g (read-graph filename))
   (define-values (dists _) (bfs g "COM"))
   (for/sum ([i (in-hash-values dists)]) i))
+
+(define (part2 filename)
+  (define g (read-graph filename))
+  (define-values (dists _) (bfs g "YOU"))
+  (- (hash-ref dists "SAN") 2))
