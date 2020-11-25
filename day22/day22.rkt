aaac2d3491c046458b60dc2f90f7418b18615a72
Dimitri Lozeve <dimitri@lozeve.com>
2020-11-25 20:01:05 +0100

    Day 22 part 1, better solution

--- day22/day22.rkt
+++ day22/day22.rkt
@@ -49,7 +49,31 @@
   (check-equal? (shuffle (range 10) (read-instructions "test4")) '(9 2 5 8 1 4 7 0 3 6)))
 
 (module+ main
-  (display "Part 1: ")
+  (displayln "Part 1:")
   (define instructions (read-instructions "input"))
   (define deck (range 10007))
-  (index-of (shuffle deck instructions) 2019))
+  (time (index-of (shuffle deck instructions) 2019)))
+
+(define (deal-card deck-size card)
+  (- (sub1 deck-size) card))
+
+(define (cut-card deck-size n card)
+  (modulo (- card n) deck-size))
+
+(define (increment-card deck-size n card)
+  (modulo (* n card) deck-size))
+
+(define (execute-card deck-size instruction card)
+  (match instruction
+    ['deal (deal-card deck-size card)]
+    [(list 'cut n) (cut-card deck-size n card)]
+    [(list 'increment n) (increment-card deck-size n card)]))
+
+(define (shuffle-card deck-size instructions card)
+  (for/fold ([c card]) ([i instructions])
+    (execute-card deck-size i c)))
+
+(module+ main
+  (displayln "Part 1 (better):")
+  (time (shuffle-card 10007 instructions 2019)))
+
