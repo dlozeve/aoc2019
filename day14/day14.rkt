7d14447feb10afe5b7b6925bfba39967d031069a
Dimitri Lozeve <dimitri@lozeve.com>
2020-11-25 18:28:07 +0100

    Day 14 part 2

--- day14/day14.rkt
+++ day14/day14.rkt
@@ -34,14 +34,17 @@
     (for ([(input input-amount) (in-hash inputs)])
       (hash-update! needed input (lambda (v) (+ v (* multiple input-amount))) 0))))
 
-(define (part1 filename)
-  (define rules (read-rules filename))
-  (define needed (make-hash '((FUEL . 1))))
+(define (ore-per-fuel rules amount)
+  (define needed (make-hash `((FUEL . ,amount))))
   (define available (make-hash))
   (for ([i (in-naturals)]
         #:break (= (apply + (hash-values needed)) (hash-ref needed 'ORE 0)))
     (generate! rules needed available))
   (hash-ref needed 'ORE))
+
+(define (part1 filename)
+  (define rules (read-rules filename))
+  (ore-per-fuel rules 1))
 
 (module+ test
   (check-eq? 31 (part1 "test1"))
@@ -53,3 +56,26 @@
 (module+ main
   (display "Part 1: ")
   (displayln (part1 "input")))
+
+(define (part2 filename)
+  (define rules (read-rules filename))
+  (define max-fuel 1000000000000)
+  (define min-fuel 1)
+  (bisect rules 1000000000000 min-fuel max-fuel))
+
+(define (bisect rules target-ore min-fuel max-fuel)
+  (define mid-fuel (floor (+ min-fuel (/ (- max-fuel min-fuel) 2))))
+  (define ore (ore-per-fuel rules mid-fuel))
+  (cond
+    [(= 1 (- max-fuel min-fuel)) min-fuel]
+    [(< ore target-ore) (bisect rules target-ore mid-fuel max-fuel)]
+    [(< target-ore ore) (bisect rules target-ore min-fuel mid-fuel)]))
+
+(module+ test
+  (check-eq? 82892753 (part2 "test3"))
+  (check-eq? 5586022 (part2 "test4"))
+  (check-eq? 460664 (part2 "test5")))
+
+(module+ main
+  (display "Part 2: ")
+  (displayln (part2 "input")))
