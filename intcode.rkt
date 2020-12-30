8b31594c6a13fa2a5b1dd770843df468ed7fe699
Dimitri Lozeve <dimitri@lozeve.com>
2020-12-30 22:21:16 +0100

    Day 9

--- intcode.rkt
+++ intcode.rkt
@@ -27,9 +27,10 @@
    5 '(jmpt 2 0)
    6 '(jmpf 2 0)
    7 '(lt 2 1)
-   8 '(eq 2 1)))
+   8 '(eq 2 1)
+   9 '(rel 1 0)))
 
-(define (parse-instruction program pc)
+(define (parse-instruction program pc relative-base)
   (define opcode (vector-ref program pc))
 
   (define-values (name nin nout)
@@ -41,29 +42,34 @@
   (for/fold ([n (quotient opcode 100)])
             ([i (in-naturals)]
              #:break (= n 0))
-    (when (< 0 (remainder n 10))
-      (hash-set! parameter-modes i (remainder n 10)))
+    (hash-set! parameter-modes i (remainder n 10))
     (quotient n 10))
 
   (define inparams
     (for/list ([i (in-range nin)])
       (match (hash-ref parameter-modes i 0)
         [0 (vector-ref program (vector-ref program (+ pc i 1)))]
-        [1 (vector-ref program (+ pc i 1))])))
+        [1 (vector-ref program (+ pc i 1))]
+        [2 (vector-ref program (+ relative-base (vector-ref program (+ pc i 1))))])))
 
   (define outparams
     (for/list ([i (in-range nout)])
-      (vector-ref program (+ pc nin i 1))))
+      (match (hash-ref parameter-modes (+ nin i) 0)
+        [0 (vector-ref program (+ pc nin i 1))]
+        [1 (error "output parameter cannot be in immediate mode" name pc)]
+        [2 (+ relative-base (vector-ref program (+ pc nin i 1)))])))
 
   (instruction name parameter-modes inparams outparams))
 
 (define (execute program inputs [pc 0])
-  (define p (vector-copy program))
+  (define p (make-vector 10000 0))
+  (vector-copy! p 0 program)
 
   (let loop ([pc pc]
              [inputs inputs]
-             [outputs '()])
-    (define instr (parse-instruction p pc))
+             [outputs '()]
+             [relative-base 0])
+    (define instr (parse-instruction p pc relative-base))
     (define inparams (instruction-inparams instr))
     (define outparams (instruction-outparams instr))
     (define next-pc (+ pc (length inparams) (length outparams) 1))
@@ -71,24 +77,25 @@
     (match (instruction-name instr)
       ['terminate (values p pc outputs #t)]
       ['add (vector-set! p (car outparams) (apply + inparams))
-            (loop next-pc inputs outputs)]
+            (loop next-pc inputs outputs relative-base)]
       ['mul (vector-set! p (car outparams) (apply * inparams))
-            (loop next-pc inputs outputs)]
+            (loop next-pc inputs outputs relative-base)]
       ['input (if (empty? inputs)
                   (values p pc outputs #f)
                   (begin
                     (vector-set! p (car outparams) (car inputs))
-                    (loop next-pc (cdr inputs) outputs)))]
-      ['output (loop next-pc inputs (cons (car inparams) outputs))]
+                    (loop next-pc (cdr inputs) outputs relative-base)))]
+      ['output (loop next-pc inputs (cons (car inparams) outputs) relative-base)]
       ['jmpt (if (not (= 0 (car inparams)))
-                 (loop (cadr inparams) inputs outputs)
-                 (loop next-pc inputs outputs))]
+                 (loop (cadr inparams) inputs outputs relative-base)
+                 (loop next-pc inputs outputs relative-base))]
       ['jmpf (if (= 0 (car inparams))
-                 (loop (cadr inparams) inputs outputs)
-                 (loop next-pc inputs outputs))]
+                 (loop (cadr inparams) inputs outputs relative-base)
+                 (loop next-pc inputs outputs relative-base))]
       ['lt (vector-set! p (car outparams)
                         (if (< (car inparams) (cadr inparams)) 1 0))
-           (loop next-pc inputs outputs)]
+           (loop next-pc inputs outputs relative-base)]
       ['eq (vector-set! p (car outparams)
                         (if (= (car inparams) (cadr inparams)) 1 0))
-           (loop next-pc inputs outputs)])))
+           (loop next-pc inputs outputs relative-base)]
+      ['rel (loop next-pc inputs outputs (+ relative-base (car inparams)))])))
