3438060385194922c16c7c53bca7db65e2c2f32a
Dimitri Lozeve <dimitri@lozeve.com>
2021-01-01 19:50:27 +0100

    Day 19 part 2

--- day19/day19.rkt
+++ day19/day19.rkt
@@ -17,3 +17,13 @@
 
 (module+ test
   (check-equal? (part1 (parse-file "input")) 229))
+
+(define (part2 program)
+  (let loop ([x 0] [y 99])
+    (cond
+      [(not (pulled? program x y)) (loop (add1 x) y)]
+      [(pulled? program (+ x 99) (- y 99)) (+ (* 10000 x) (- y 99))]
+      [else (loop x (add1 y))])))
+
+(module+ test
+  (check-equal? (part2 (parse-file "input")) 6950903))
