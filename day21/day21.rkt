948de0b851168fa2e6ff3946d954566d676d6600
Dimitri Lozeve <dimitri@lozeve.com>
2021-01-02 11:08:09 +0100

    Day 21

---
+++ day21/day21.rkt
@@ -0,0 +1,43 @@
+#lang racket
+
+(require "../intcode.rkt")
+
+(module+ test
+  (require rackunit))
+
+(define (part1 program)
+  (define inputs #<<EOF
+OR A J
+AND B J
+AND C J
+NOT J J
+AND D J
+WALK
+
+EOF
+    )
+  (define vm (execute (start-machine program inputs)))
+  (car (machine-outputs vm)))
+
+(module+ test
+  (check-equal? (part1 (parse-file "input")) 19359533))
+
+(define (part2 program)
+  (define inputs #<<EOF
+OR A J
+AND B J
+AND C J
+NOT J J
+AND D J
+OR E T
+OR H T
+AND T J
+RUN
+
+EOF
+    )
+  (define vm (execute (start-machine program inputs)))
+  (car (machine-outputs vm)))
+
+(module+ test
+  (check-equal? (part2 (parse-file "input")) 1140310551))
