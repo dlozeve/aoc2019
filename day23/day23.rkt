11bdcbcd3f6f814bfe88236f2cd9743823843785
Dimitri Lozeve <dimitri@lozeve.com>
2021-01-06 09:20:35 +0100

    Day 23 part 1

---
+++ day23/day23.rkt
@@ -0,0 +1,28 @@
+#lang racket
+
+(require "../intcode.rkt")
+
+(module+ test
+  (require rackunit))
+
+(define (get-packets vms)
+  (let loop ([packets (reverse (append* (map machine-outputs vms)))]
+             [h (hash)])
+    (match packets
+      ['() h]
+      [(list* addr x y r) (loop r (hash-update h addr (λ (lst) (append (list x y) lst)) '()))])))
+
+(define (part1 program)
+  (define vms (for/list ([i (in-range 50)])
+                (execute (start-machine program (list i)))))
+  (let loop ([vms vms])
+    (define packets (get-packets vms))
+    (if (hash-has-key? packets 255)
+        (cadr (hash-ref packets 255))
+        (loop
+         (for/list ([i (in-range 50)]
+                    [vm (in-list vms)])
+           (execute (struct-copy machine vm [inputs (hash-ref packets i '(-1))])))))))
+
+(module+ test
+  (check-equal? (part1 (parse-file "input")) 23266))
